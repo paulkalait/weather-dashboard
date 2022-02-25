fa37c324fbc598f7d3fbac3564b2367cfd028420
Your Name <kalaitzidispaul@gmail.com>
2022-02-24 19:37:00 -0500

    added imperial

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 var apiKey = "aa4bb6474b2954b1c9b7ca7e977ee0df"
-var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=" + apiKey
+//gives you the lat and long
+var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=" + apiKey + "&units=imperial"
 var lat;
 var long;
 //one day gives lat long  
 
-var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + citySearch + "&appid=" + apiKey
+//gives you the city look up 
+var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + citySearch + "&appid=" + apiKey + "&units=imperial"
 var citySearch = boston;
 
 
@@ -13,6 +15,15 @@ fetch(apiUrl)
     console.log(apiUrl)
     return response.json();
   })
+
+  //nest the onecall in a .then
   .then(function (data) {
     console.log(data)
+
+    //[0].lat.long 
+
+
   });
+
+
+  //subset api is the one call 
